32b833ecce498d169201ac34f156610fe4e347ad
simo994q <simonkn1@hotmail.com>
2023-03-16 08:47:36 +0100

    fpwiefwe

--- assets/js.js
+++ assets/js.js
@@ -1,13 +1,18 @@
+const header = document.getElementById('header')
 const myFadeImg = document.getElementsByClassName("fadeImg");
+const aboutSection = document.getElementById('omDeGule')
+const footer = document.getElementById('footer')
 
 
 window.addEventListener('load', () => {
     console.log('loaded');
-
-
     for (const myElements of myFadeImg){
         myElements.classList.toggle('showImage');
     };
+    header.classList.add('')
+    aboutSection.classList.add('')
+    footer.classList.add('')
+
 });
 
 
@@ -15,8 +20,6 @@ const button = document.getElementById('showButton');
 const text = document.getElementsByClassName('sectionText');
 
 button.addEventListener('click', () => {
-    console.log(3);
-    console.log(text);
     for (const myElements of text){
         myElements.classList.toggle('textShow');
     };
